0167d7f6da8655320037f6374db6a227219c6ba8
Costa Fotiadis <con.fotiadis@gmail.com>
2026-04-10 15:52:54 +0300

    Initial commit

---
+++ AdbExtension/AdbExtension.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.CommandPalette.Extensions;
+
+namespace AdbExtension;
+
+[Guid("d857a76b-60ad-4db5-a14c-22f1d4f7bfaa")]
+public sealed partial class AdbExtension : IExtension, IDisposable
+{
+    private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly AdbExtensionCommandsProvider _provider = new();
+
+    public AdbExtension(ManualResetEvent extensionDisposedEvent)
+    {
+        this._extensionDisposedEvent = extensionDisposedEvent;
+    }
+
+    public object? GetProvider(ProviderType providerType)
+    {
+        return providerType switch
+        {
+            ProviderType.Commands => _provider,
+            _ => null,
+        };
+    }
+
+    public void Dispose() => this._extensionDisposedEvent.Set();
+}
