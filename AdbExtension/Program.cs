b8c35320b093f9321fd18e0f6b6b337811a863b9
Costa Fotiadis <con.fotiadis@gmail.com>
2026-04-16 16:14:50 +0300

    Fix COM server crash + add error logging (v1.0.0.2) (#8)

--- AdbExtension/Program.cs
+++ AdbExtension/Program.cs
@@ -17,22 +17,25 @@ public class Program
 
         if (args.Length > 0 && args[0] == "-RegisterProcessAsComServer")
         {
-            global::Shmuelie.WinRTServer.ComServer server = new();
-
-            ManualResetEvent extensionDisposedEvent = new(false);
-            
-            // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
-            // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
-            // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
-            AdbExtension extensionInstance = new(extensionDisposedEvent);
-            server.RegisterClass<AdbExtension, IExtension>(() => extensionInstance);
-            server.Start();
-            
-            // This will make the main thread wait until the event is signalled by the extension class.
-            // Since we have single instance of the extension object, we exit as soon as it is disposed.
-            extensionDisposedEvent.WaitOne();
-            server.Stop();
-            server.UnsafeDispose();
+            Log.Info("RegisterProcessAsComServer mode detected.");
+            try
+            {
+                global::Shmuelie.WinRTServer.ComServer server = new();
+                ManualResetEvent extensionDisposedEvent = new(false);
+                AdbExtension extensionInstance = new(extensionDisposedEvent);
+                server.RegisterClass<AdbExtension, IExtension>(() => extensionInstance);
+                Log.Info("COM server registered. Starting...");
+                server.Start();
+                Log.Info("COM server started. Waiting for disposal signal.");
+                extensionDisposedEvent.WaitOne();
+                Log.Info("Disposal signal received. Stopping server.");
+                server.Stop();
+                server.UnsafeDispose();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("COM server failed", ex);
+            }
         }
         else
         {
